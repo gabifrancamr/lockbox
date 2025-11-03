41f84fc77a05b3af2fe0c5ba5c1337a24239a731
Gabriela <gabifmouraroc@outlook.com>
2025-11-03 16:50:01 -0300

    Criando uma nota

--- views/template/app.php
+++ views/template/app.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en" data-theme="dracula">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,15 +12,22 @@
 
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-<body>
-    <div class="mx-auto max-w-screen-lg h-screen flex flex-col">
-        <?php require base_path('views/partials/_navbar.view.php') ?>
 
-        <?php require base_path('views/partials/_pesquisar.view.php') ?>
-        
+<body>
+    <div class="mx-auto max-w-screen-lg h-screen flex flex-col space-y-4">
+
+        <div>
+            <?php require base_path('views/partials/_navbar.view.php') ?>
+
+            <?php require base_path('views/partials/_pesquisar.view.php') ?>
+        </div>
+
+        <?php require base_path('views/partials/_mensagem.view.php') ?>
+
         <div class="flex flex-grow py-6">
             <?php require "../views/{$view}.view.php"; ?>
         </div>
     </div>
 </body>
+
 </html>
