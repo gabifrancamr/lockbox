d19d8b61ff191556aa88680dc22e68951a9f6f1d
Gabriela <gabifmouraroc@outlook.com>
2025-11-03 17:36:39 -0300

    Listando todas as notas

--- App/Controllers/Notas/IndexController.php
+++ App/Controllers/Notas/IndexController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controllers\Notas;
 
+use App\Models\Nota;
+
 class IndexController {
     public function __invoke()
     {
-        if(!auth()) {
-            redirect('/login');
+        $notas = Nota::all();
 
-            exit();
-        }
+        $id = isset($_GET['id']) ? $_GET['id'] : $notas[0]->id; 
+
+        $filtro = array_filter($notas, fn($n) => $n->id == $id);
+
+        $notaSelecionada = array_pop($filtro);
 
         return view('notas', [
-            'user' => auth()
+            'notas' => $notas,
+            'notaSelecionada' => $notaSelecionada
         ]);
     }
 }
